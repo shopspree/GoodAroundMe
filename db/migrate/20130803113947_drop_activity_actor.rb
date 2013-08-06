a342e44c8e80ebabfa9460444ada0c67eba9299f
Asaf A.M <asaf.ach@gmail.com>
2013-08-07 00:30:04 +0300

    db migration scripts fix

--- db/migrate/20130803113947_drop_activity_actor.rb
+++
@@ -1,8 +0,0 @@
-class DropActivityActor < ActiveRecord::Migration
-  def up
-    drop_table :activity_actors
-  end
-
-  def down
-  end
-end
--- db/migrate/20130803145525_drop_action.rb
+++
@@ -1,8 +0,0 @@
-class DropAction < ActiveRecord::Migration
-  def up
-    drop_table :actions
-  end
-
-  def down
-  end
-end
