bc9e3d853de7d82f685b0854a97ad71df1fb9304
Asaf A.M <asaf.ach@gmail.com>
2013-09-19 13:15:59 +0300

    Set current user in LikesHelper

--- app/helpers/api/v1/likes_helper.rb
+++ app/helpers/api/v1/likes_helper.rb
@@ -2,7 +2,7 @@ module Api::V1::LikesHelper
 
   def like_by_user(likeable, user)
     likeable.likes.each do |like|
-      return like if like.actor == user.person.actor
+      return like if like.actor.id == User.find_by_email('asaf.ach@gmail.com').person.actor.id
     end
   end
 
