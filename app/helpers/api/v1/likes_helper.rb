435a62a50a236b48e0e83d04cfbb175b81070022
Asaf A.M <asaf.ach@gmail.com>
2013-09-19 13:26:23 +0300

    improve log

--- app/helpers/api/v1/likes_helper.rb
+++ app/helpers/api/v1/likes_helper.rb
@@ -1,9 +1,10 @@
 module Api::V1::LikesHelper
 
   def like_by_user(likeable, user)
+    logger.debug("[DEBUG] <LikesHelper> in like_by_user")
     user_actor_id = User.find_by_email('asaf.ach@gmail.com').person.actor.id
     likeable.likes.each do |like|
-      logger.debug("[DEBUG] <LikesController> #{ @likeable.id} like actor is #{like.actor.id} while current_user actor is #{user_actor_id}")
+      logger.debug("[DEBUG] <LikesHelper> #{ @likeable.id} like actor is #{like.actor.id} while current_user actor is #{user_actor_id}")
       return like if like.actor.id == user_actor_id
     end
   end
