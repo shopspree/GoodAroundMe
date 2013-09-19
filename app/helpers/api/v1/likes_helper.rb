06dac2dc76a238faca19999b830292d2d2265676
Asaf A.M <asaf.ach@gmail.com>
2013-09-19 14:17:25 +0300

    fix

--- app/helpers/api/v1/likes_helper.rb
+++ app/helpers/api/v1/likes_helper.rb
@@ -1,10 +1,10 @@
 module Api::V1::LikesHelper
 
   def like_by_user(likeable, user)
-    user_actor_id = user.person.actor_id
+    user_actor_id = user.person.actor.id
 
     likeable.likes.each do |like|
-      logger.debug("[DEBUG] <LikesHelper> like #{like.id} actor is #{like.actor_id} while current_user actor is #{user_actor_id}")
+      logger.debug("[DEBUG] <LikesHelper> like #{like.id} actor is #{like.actor.id} while current_user actor is #{user_actor_id}")
       if like.actor_id == user_actor_id
         return like
       end
