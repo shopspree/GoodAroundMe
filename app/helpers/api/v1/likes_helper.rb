f5a40cc6099b509f25a5233ea35116223e48986f
Asaf A.M <asaf.ach@gmail.com>
2013-09-19 14:28:06 +0300

    improve performane by stroring current_user actor id before loop

--- app/helpers/api/v1/likes_helper.rb
+++ app/helpers/api/v1/likes_helper.rb
@@ -3,8 +3,9 @@ module Api::V1::LikesHelper
   def like_by_user(likeable, user)
     like_by_user = nil
 
+    current_actor_id =  user.person.actor.id
     likeable.likes.each do |like|
-      if like.actor_id == user.person.actor.id
+      if like.actor_id ==  current_actor_id
         like_by_user = like
         break
       end
