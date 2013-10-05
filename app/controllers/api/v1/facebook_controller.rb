3801f343bf8e2d3c2a158e72363af4d6327e5a3e
Asaf A.M <asaf.ach@gmail.com>
2013-10-05 19:20:10 +0300

    Fix callback

--- app/controllers/api/v1/facebook_controller.rb
+++ app/controllers/api/v1/facebook_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::FacebookController < ApplicationController
 
-  # POST /api/v1/facebook/callback.json
+  # POST /api/v1/facebook/callback
   def callback
-    fb_mode = params[hub.mode]
-    fb_verify_token = params[hub.verify_token]
-    fb_challenge = params[hub.challenge]
+    fb_mode = params["hub.mode"]
+    fb_verify_token = params["hub.verify_token"]
+    @fb_challenge = params["hub.challenge"]
     logger.debug("[DEBUG] <FacebookController> Callback success for token") if fb_verify_token == 'asaf'
     logger.debug("[DEBUG] <FacebookController> callback: with params #{params}")
   end
