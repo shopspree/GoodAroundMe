0492aa477412188bb3578ce4b3cb0ea3d3f60d31
Asaf A.M <asaf.ach@gmail.com>
2013-10-06 09:29:14 +0200

    Fix facebook controller to return plain text

--- app/controllers/api/v1/facebook_controller.rb
+++ app/controllers/api/v1/facebook_controller.rb
@@ -4,9 +4,12 @@ class Api::V1::FacebookController < ApplicationController
   def callback
     fb_mode = params["hub.mode"]
     fb_verify_token = params["hub.verify_token"]
-    @fb_challenge = params["hub.challenge"]
-    logger.debug("[DEBUG] <FacebookController> Callback success for token") if fb_verify_token == 'asaf'
-    logger.debug("[DEBUG] <FacebookController> callback: with params #{params}")
+    fb_challenge = params["hub.challenge"]
+
+    logger.debug("[DEBUG] <Api::V1::FacebookController> Callback success for token") if fb_verify_token == 'asaf'
+    logger.debug("[DEBUG] <Api::V1::FacebookController> callback: with params #{params}")
+
+    render text: fb_challenge
   end
 
 end
