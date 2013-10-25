bb2718e0f08906e249580cb161f641c417f02789
Asaf A.M <asaf.ach@gmail.com>
2013-10-25 14:43:59 -0400

    metadata controller

--- app/controllers/api/v1/metadata_controller.rb
+++ app/controllers/api/v1/metadata_controller.rb
@@ -3,10 +3,12 @@ class Api::V1::MetadataController < API::V1::BaseController
   # GET /api/v1/metadata.json
   def index
 
-    @amazon_s3_id
-    @amazon_s3_secret_id = ENV[""]
+    @amazon_s3_app_id = ENV["AMAZON_S3_APP_ID"]
+    @amazon_s3_app_secret = ENV["AMAZON_S3_APP_SECRET"]
 
-    @give_url
+    @give_url = 'http://www.goodaround.me'
+
+    @image_size_bytes_threshold = 100000
 
   end
 
