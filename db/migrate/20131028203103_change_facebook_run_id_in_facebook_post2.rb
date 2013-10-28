2a13ed13c4b23ee42a30decb4333b655b2a841d7
Asaf A.M <asaf.ach@gmail.com>
2013-10-28 16:40:54 -0400

    remove specific migration for PG
    Change was done on original table creation migration

--- db/migrate/20131028203103_change_facebook_run_id_in_facebook_post2.rb
+++
@@ -1,9 +0,0 @@
-class ChangeFacebookRunIdInFacebookPost2 < ActiveRecord::Migration
-  def up
-    change_column :facebook_posts, :facebook_run_id, 'integer USING CAST(facebook_run_id AS integer)'
-  end
-
-  def down
-    change_column :facebook_posts, :facebook_run_id, 'string USING CAST(facebook_run_id AS string)'
-  end
-end
