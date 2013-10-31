f0dd634c0106561439757f999960c5d8416fe040
Asaf A.M <asaf.ach@gmail.com>
2013-10-31 12:11:22 -0400

    SMTP email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,15 @@ Goodaroundme::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' } #TODO!!!
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' } #TODO!!!
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.office365.com',
+      :port           => '587',
+      :authentication => :login,
+      :user_name      => ENV['SMTP_USERNAME'],
+      :password       => ENV['SMTP_PASSWORD'],
+      :domain         => 'goodaround.me',
+      :enable_starttls_auto => true
+  }
 end
