e4feb3cc025bfb6ad58b23f7523b9083e26eae61
Asaf A.M <asaf.ach@gmail.com>
2013-11-04 14:31:19 -0500

    create method for json controller

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::PasswordsController < Devise::PasswordsController
 
   respond_to :json
 
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+
+    espond_with self.resource.errors, status: :unprocessable_entity unless successfully_sent?(resource)
+
+
+  end
+
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
     #return your path
