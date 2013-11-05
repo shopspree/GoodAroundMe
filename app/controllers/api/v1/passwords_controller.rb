1feb98095810c15243add77e39ba27e342ff05a7
Asaf A.M <asaf.ach@gmail.com>
2013-11-05 17:43:42 -0500

    Dot instead of comma in hostname

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -13,10 +13,4 @@ class Api::V1::PasswordsController < Devise::PasswordsController
     end
 
   end
-
-  protected
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    #return your path
-    Rails.logger.info "\n\n\nSent password reset email\n\n"
-  end
 end
