8d7786036e5e9f22ca3c54c11533d067ceb32643
Asaf A.M <asaf.ach@gmail.com>
2013-11-26 12:42:12 +0200

    setting default bucket prefix path

--- app/controllers/api/v1/metadata_controller.rb
+++ app/controllers/api/v1/metadata_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::MetadataController < Api::V1::BaseController
 
     @amazon_s3_app_id = ENV["AMAZON_S3_APP_ID"]
     @amazon_s3_app_secret = ENV["AMAZON_S3_APP_SECRET"]
-    @amazon_s3_bucket = "";
+    @amazon_s3_bucket = "GoodAroundMe/media";
 
     @give_enable = false
     @give_url = 'http://www.goodaround.me'
