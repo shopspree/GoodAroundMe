64b10b4b41957244658d1b77500751bd8104a85f
Asaf A.M <asaf.ach@gmail.com>
2013-11-26 12:41:59 +0200

    users js testing

---
+++ app/assets/javascripts/admin/users.js
@@ -0,0 +1,23 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+function approve() {
+    alert("hello world");
+}
+
+
+#function checkAll(check_all){
+#// Loop through an array containing ALL inputs on same form as check_all
+#        var inputs = check_all.form.getElementsByTagName("input");
+#        for (var i = 0; i < inputs.length; i++) {
+#// Only work on checkboxes, and NOT on the "Check All" checkbox
+#            if (inputs[i].type == "checkbox" && inputs[i].name != check_all.name) {
+#                if(check_all.checked == true){
+#                    inputs[i].checked = true ;
+#                }else{
+#                    inputs[i].checked = false ;
+#                }
+#            }
+#        }
+#    }
